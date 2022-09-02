520df59ffce96504ed0c88c662736e39a92722aa
Manuel2u <edodoo011@st.ug.edu.gh>
2022-09-02 09:38:27 +0000

    Added mongoose connection

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 app.set('view engine', 'ejs');
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 const date = require(__dirname + "/date.js");
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-let today = date(); 
+let today = date();
 
 let list = [];
 
-app.get("/", function(req,res){
-   res.render("index",{kindOfDay:today, newITem:list});
+app.get("/", function (req, res) {
+  res.render("index", {
+    kindOfDay: today,
+    newITem: list
+  });
 });
 
-app.post("/", function(req,res){
+app.post("/", function (req, res) {
   let newItem = req.body.new_item;
   list.push(newItem);
   res.redirect("/");
@@ -33,6 +38,6 @@ app.post("/", function(req,res){
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server Started at Port 3000...");
 });
