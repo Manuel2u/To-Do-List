e6599162af15570053bd3d9b60005c51d35057b9
Manuel2u <edodoo011@st.ug.edu.gh>
2022-09-03 14:13:42 +0000

    Half way through

--- app.js
+++ app.js
@@ -8,40 +8,71 @@ app.use(express.static("public"));
 const date = require(__dirname + "/date.js");
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-const listSchema = new mongoose.Schema({
-  item : "string"
-});
+const listSchema = {
+  name: "string"
+};
 
 const List = mongoose.model('list', listSchema);
+
+let item1 = new List({
+  name: "Welcome to your ToDo-List"
+});
+
+let item2 = new List({
+  name: "Hit the + button to add a new item"
+});
+
+
+let defaultItems = [item1, item2];
+
 
 let today = date();
 
 let list = [];
 
 app.get("/", function (req, res) {
-  res.render("index", {
-    kindOfDay: today,
-    newITem: list
+  List.find({}, function (err, foundItems) {
+    if (foundItems.length === 0) {
+      List.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("index", {
+        kindOfDay: today,
+        newListItems: foundItems
+      });
+    }
   });
 });
 
 app.post("/", function (req, res) {
   let newItem = req.body.new_item;
-  list.push(newItem);
+
+  let newListItem = new List({
+    name: newItem
+  });
+
+  newListItem.save();
+
   res.redirect("/");
 });
 
 
-
-
-
-
-
-
-
-
-
-
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox;
+  console.log(checkedItemId);
+  List.findByIdAndRemove(checkedItemId, function (err) {
+    if (!err) {
+      console.log("Successfully deleted checked item");
+      res.redirect("/");
+    }
+  });
+});
 
 
 app.listen(3000, function () {
